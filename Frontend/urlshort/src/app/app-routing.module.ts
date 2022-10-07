240d6238f2660e65b70c9923a1421da3f9983fd7
VivekanDev <Vivekananthan.samiyappan@ilink-systems.com>
2022-10-07 22:38:09 +0530

    routing updated.

--- Frontend/urlshort/src/app/app-routing.module.ts
+++ Frontend/urlshort/src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { MainPageComponent } from './main-page/main-page.component';
 const routes: Routes = [
   {
     path:"", component:MainPageComponent
+  },
+  {
+    path:"**", redirectTo:"",
   }
 ];
 
