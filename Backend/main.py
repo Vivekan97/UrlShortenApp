1ebd37609960bef4c45569760da0b71a92fd1429
VivekanDev <Vivekananthan.samiyappan@ilink-systems.com>
2022-10-07 21:59:26 +0530

    backend codes pushed.

---
+++ Backend/main.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from shortener import get_shortened_url, get_long_url
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/", methods=["GET"])
+def homepage():
+    return jsonify(message="Welcome to URL Shortener")
+
+
+@app.route("/short", methods=["POST"])
+def short_url():
+    source = request.get_json()
+    
+    if "url" not in source.keys():
+        return jsonify(input=None, result=None, message="Invalid URL. Cannot process !", timestamp=datetime.now()), 400
+    
+    source_url = source["url"]
+    short = get_shortened_url(source_url)
+    if short is None:
+        return jsonify(input=source_url, result=short, message="Invalid URL. Cannot process !", timestamp=datetime.now()), 400
+    return jsonify(input=source_url, result=short, timestamp=datetime.now())
+
+
+@app.route("/long", methods=["POST"])
+def long_url():
+    source = request.get_json()
+    
+    if "url" not in source.keys():
+        return jsonify(input=None, result=None, message="Invalid URL. Cannot process !", timestamp=datetime.now()), 400
+    
+    source_url = ["url"]
+    long = get_long_url(source_url)
+    if long is None:
+        return jsonify(input=source_url, result=long, message="Invalid URL. Cannot process !", timestamp=datetime.now()), 400
+    return jsonify(input=source_url, result=long, timestamp=datetime.now())
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
